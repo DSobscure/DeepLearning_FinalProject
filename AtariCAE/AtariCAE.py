0950cb63f7208de13ad9f0ca87b4e9100f8fdded
DSobscure <snbruce13@yahoo.com.tw>
2017-06-25 16:38:08 +0800

    !

--- AtariCAE/AtariCAE.py
+++ AtariCAE/AtariCAE.py
@@ -60,80 +60,64 @@ class CAE():
             self.rec_states.append(rec_state)
             self.original_states.append(self.original_states[i] - rec_state)
         
-        self.rec_loss1 = tf.reduce_mean(tf.pow(self.rec_states[0] - self.original_states[0], 2))
-        self.rec_loss2 = tf.reduce_mean(tf.pow(self.rec_states[1] - self.original_states[1], 2))
+        self.rec_loss = tf.reduce_mean(tf.pow(self.rec_states[0] - (self.original_states[0] + self.rec_states[1]), 2))
         
-        self.optimize1 = tf.train.RMSPropOptimizer(0.00025).minimize(self.rec_loss1)  
-        self.optimize2 = tf.train.RMSPropOptimizer(0.00025).minimize(self.rec_loss2)  
+        self.optimize = tf.train.RMSPropOptimizer(0.00025).minimize(self.rec_loss)  
 
         self.rec_state =  self.rec_states[0] + self.rec_states[1]
         print(self.rec_state.get_shape())
 
     def build_network(self, x, trainable=True):
-        conv1_weight = tf.Variable(tf.truncated_normal([8, 8, 4, 32], stddev = 0.02), trainable = trainable)
-        conv1_bias = tf.Variable(tf.constant(0.02, shape = [32]), trainable = trainable)             
+        conv1_weight = tf.Variable(tf.truncated_normal([8, 8, 4, 16], stddev = 0.02), trainable = trainable)
+        conv1_bias = tf.Variable(tf.constant(0.02, shape = [16]), trainable = trainable)             
         conv1_hidden_sum = tf.nn.conv2d(x, conv1_weight, strides = [1,4,4,1], padding='SAME') + conv1_bias      
         conv1_hidden_bn = batch_norm(conv1_hidden_sum)        
         conv1_hidden = tf.nn.elu(conv1_hidden_bn)
 
-        conv2_weight = tf.Variable(tf.truncated_normal([4, 4, 32, 64], stddev = 0.02), trainable = trainable)
-        conv2_bias = tf.Variable(tf.constant(0.02, shape = [64]), trainable = trainable)
+        conv2_weight = tf.Variable(tf.truncated_normal([4, 4, 16, 32], stddev = 0.02), trainable = trainable)
+        conv2_bias = tf.Variable(tf.constant(0.02, shape = [32]), trainable = trainable)
         conv2_hidden_sum = tf.nn.conv2d(conv1_hidden, conv2_weight, strides = [1,2,2,1], padding='SAME') + conv2_bias
         conv2_hidden_bn = batch_norm(conv2_hidden_sum)
         conv2_hidden = tf.nn.elu(conv2_hidden_bn)
 
-        conv3_weight = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev = 0.02), trainable = trainable)
-        conv3_bias = tf.Variable(tf.constant(0.02, shape = [64]), trainable = trainable)       
+        conv3_weight = tf.Variable(tf.truncated_normal([3, 3, 32, 32], stddev = 0.02), trainable = trainable)
+        conv3_bias = tf.Variable(tf.constant(0.02, shape = [32]), trainable = trainable)       
         conv3_hidden_sum = tf.nn.conv2d(conv2_hidden, conv3_weight, strides = [1,1,1,1], padding='SAME') + conv3_bias
         conv3_hidden_bn = batch_norm(conv3_hidden_sum)
         conv3_hidden = tf.nn.elu(conv3_hidden_bn)
 
-        conv3_hidden_flat = tf.reshape(conv3_hidden, [-1, 11*11*64])
-        fc1_weight = tf.Variable(tf.truncated_normal([11*11*64, 512], stddev = 0.02), trainable = trainable)
-        fc1_bias = tf.Variable(tf.constant(0.02, shape = [512]), trainable = trainable)      
+        conv3_hidden_flat = tf.reshape(conv3_hidden, [-1, 11*11*32])
+        fc1_weight = tf.Variable(tf.truncated_normal([11*11*32, CODE_SIZE], stddev = 0.02), trainable = trainable)
+        fc1_bias = tf.Variable(tf.constant(0.02, shape = [CODE_SIZE]), trainable = trainable)      
         fc1_hidden_sum = tf.matmul(conv3_hidden_flat, fc1_weight) + fc1_bias
         fc1_hidden_bn = batch_norm(fc1_hidden_sum)
-        fc1_hidden = tf.nn.elu(fc1_hidden_bn)
+        fc1_hidden = tf.nn.sigmoid(fc1_hidden_bn)
 
-        fc2_weight = tf.Variable(tf.truncated_normal([512, CODE_SIZE], stddev = 0.02), trainable = trainable)
-        fc2_bias = tf.Variable(tf.constant(0.02, shape = [CODE_SIZE]), trainable = trainable)      
-        fc2_hidden_sum = tf.matmul(fc1_hidden, fc2_weight) + fc2_bias
-        fc2_hidden_bn = batch_norm(fc2_hidden_sum)
-        fc2_hidden = tf.nn.tanh(fc2_hidden_bn)
-
-        code_layer = fc2_hidden
+        code_layer = fc1_hidden
         print("code layer shape : %s" % code_layer.get_shape())
 
-        dfc1_weight = tf.Variable(tf.truncated_normal([CODE_SIZE, 512], stddev = 0.02))
-        dfc1_bias = tf.Variable(tf.constant(0.02, shape = [512]))
-        dfc1_hidden_sum = tf.matmul(fc2_hidden, dfc1_weight) + dfc1_bias
-        dfc1_hidden_bn = batch_norm(dfc1_hidden_sum)
-        dfc1_hidden = tf.nn.elu(dfc1_hidden_bn)
-
-        print("dcode layer shape : %s" % dfc1_hidden.get_shape())
-
-        dfc2_weight = tf.Variable(tf.truncated_normal([512, 11*11*64], stddev = 0.02))
-        dfc2_bias = tf.Variable(tf.constant(0.02, shape = [11*11*64]))
-        dfc2_hidden_sum = tf.matmul(dfc1_hidden, dfc2_weight) + dfc2_bias
+        dfc2_weight = tf.Variable(tf.truncated_normal([CODE_SIZE, 11*11*32], stddev = 0.02))
+        dfc2_bias = tf.Variable(tf.constant(0.02, shape = [11*11*32]))
+        dfc2_hidden_sum = tf.matmul(code_layer, dfc2_weight) + dfc2_bias
         dfc2_hidden_bn = batch_norm(dfc2_hidden_sum)
         dfc2_hidden = tf.nn.elu(dfc2_hidden_bn)
-        dfc2_hidden_conv = tf.reshape(dfc2_hidden, [-1, 11, 11, 64])
+        dfc2_hidden_conv = tf.reshape(dfc2_hidden, [-1, 11, 11, 32])
 
-        dconv1_weight = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev = 0.02), trainable = trainable)
-        dconv1_bias = tf.Variable(tf.constant(0.02, shape = [64]), trainable = trainable)
-        dconv1_output_shape = tf.stack([tf.shape(x)[0], 11, 11, 64])
+        dconv1_weight = tf.Variable(tf.truncated_normal([3, 3, 32, 32], stddev = 0.02), trainable = trainable)
+        dconv1_bias = tf.Variable(tf.constant(0.02, shape = [32]), trainable = trainable)
+        dconv1_output_shape = tf.stack([tf.shape(x)[0], 11, 11, 32])
         dconv1_hidden_sum = tf.nn.conv2d_transpose(dfc2_hidden_conv, dconv1_weight, dconv1_output_shape, strides = [1,1,1,1], padding='SAME') + dconv1_bias
         dconv1_hidden_bn = batch_norm(dconv1_hidden_sum)
         dconv1_hidden = tf.nn.elu(dconv1_hidden_bn)
 
-        dconv2_weight = tf.Variable(tf.truncated_normal([4, 4, 32, 64], stddev = 0.02), trainable = trainable)
-        dconv2_bias = tf.Variable(tf.constant(0.02, shape = [32]), trainable = trainable)
-        dconv2_output_shape = tf.stack([tf.shape(x)[0], 21, 21, 32])
+        dconv2_weight = tf.Variable(tf.truncated_normal([4, 4, 16, 32], stddev = 0.02), trainable = trainable)
+        dconv2_bias = tf.Variable(tf.constant(0.02, shape = [16]), trainable = trainable)
+        dconv2_output_shape = tf.stack([tf.shape(x)[0], 21, 21, 16])
         dconv2_hidden_sum = tf.nn.conv2d_transpose(dconv1_hidden, dconv2_weight, dconv2_output_shape, strides = [1,2,2,1], padding='SAME') + dconv2_bias
         dconv2_hidden_bn = batch_norm(dconv2_hidden_sum)
         dconv2_hidden = tf.nn.elu(dconv2_hidden_bn)
 
-        dconv3_weight = tf.Variable(tf.truncated_normal([8, 8, 4, 32], stddev = 0.02), trainable = trainable)
+        dconv3_weight = tf.Variable(tf.truncated_normal([8, 8, 4, 16], stddev = 0.02), trainable = trainable)
         dconv3_bias = tf.Variable(tf.constant(0.02, shape = [4]), trainable = trainable)
         dconv3_output_shape = tf.stack([tf.shape(x)[0], 84, 84, 4])
         dconv3_hidden_sum = tf.nn.conv2d_transpose(dconv2_hidden, dconv3_weight, dconv3_output_shape, strides = [1,4,4,1], padding='SAME') + dconv3_bias
@@ -141,7 +125,7 @@ class CAE():
         return dconv3_hidden_sum
 
     def update_code(self, sess, state):
-        sess.run([self.optimize1, self.optimize2], feed_dict={self.state : state})
+        sess.run([self.optimize], feed_dict={self.state : state})
 
     #def get_code(self, state):
     #    outputs = self.code_output.eval(feed_dict={self.state: state})
@@ -211,8 +195,7 @@ def main(_):
             print("generate code...", i)
             #print(cae.get_code(samples))
             #print(cae.get_code([initial_state]))
-            print(cae.rec_loss1.eval(feed_dict={cae.state: samples}))
-            print(cae.rec_loss2.eval(feed_dict={cae.state: samples}))
+            print(cae.rec_loss.eval(feed_dict={cae.state: samples}))
         if (i) % 5000 == 0:
             plot_n_reconstruct(samples, cae.rec_states[0].eval(feed_dict={cae.state: samples}))
             plot_n_reconstruct(samples, cae.rec_states[1].eval(feed_dict={cae.state: samples}))
